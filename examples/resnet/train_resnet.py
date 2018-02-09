b102a342ab1531ca88b89905fd3d67f7ff6e72bc
Jennifer Myers <jennifer.myers@intel.com>
2018-02-08 18:11:27 -0800

    Update copyright from Nervana to Intel

--- examples/resnet/train_resnet.py
+++ examples/resnet/train_resnet.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
-# ----------------------------------------------------------------------------
-# Copyright 2017 Nervana Systems Inc.
+# ******************************************************************************
+# Copyright 2017-2018 Intel Corporation
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ----------------------------------------------------------------------------
+# ******************************************************************************
+
+# HETR Usage:
+# - set environment variables for HETR_AEON_IP and HETR_AEON_PORT
+# - run the aeon-service on antoher linux terminal
+#   - cd <PATH_TO_AEON_SERVICE>
+#   - ./aeon-service --uri http://<HETR_AEON_IP>:<HETR_AEON_PORT>
+# - launch train_resnet.py with the desired arg parameters
+
 from __future__ import division, print_function
 import numpy as np
 import ngraph as ng
