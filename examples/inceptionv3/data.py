93687f8c3a2ae409f37eea09ea1d92c11e89bd07
Sang Ik Lee <sang.ik.lee@intel.com>
2018-03-08 15:24:51 -0800

    Fix broken import for inceptionv3.

--- examples/inceptionv3/data.py
+++ examples/inceptionv3/data.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ******************************************************************************
-from neon.op.frontend.aeon_shim import AeonDataLoader
-from neon.op.util.persist import get_data_cache_or_nothing
+from neon.frontend.aeon_shim import AeonDataLoader
+from neon.util.persist import get_data_cache_or_nothing
 
 
 def make_aeon_loaders(train_manifest, valid_manifest,
