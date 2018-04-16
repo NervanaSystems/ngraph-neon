b7f2b5392fa5fb9ea3f85200f2d1555a997a8a7d
Sang Ik Lee <sang.ik.lee@intel.com>
2018-04-16 14:25:02 -0700

    Add a script that installs neon with nGraph and aeon into virtualenvs for python2 and python3.

---
+++ contrib/neon/install_cpu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# ******************************************************************************
+# Copyright 2017-2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ******************************************************************************
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+${SCRIPT_DIR}/neon_cpu_wheels.sh
+
+git clone -b v1.3.1 https://github.com/NervanaSystems/aeon.git
+
+. .venv2/bin/activate && mkdir -p aeon/build2 && pushd aeon/build2 && cmake .. && pip install -U . && deactivate && popd
+. .venv3/bin/activate && mkdir -p aeon/build3 && pushd aeon/build3 && cmake .. && pip install -U . && deactivate && popd
+
+echo "neon for python2 installed in virtualenv .venv2"
+echo "neon for python3 installed in virtualenv .venv3"
